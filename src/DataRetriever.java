d6b1a702332d55c76225e14a48a73d5ae87c6e57
14chanwa <spam-cwn@hotmail.com>
2018-01-24 19:34:00 +0100

    Implemented departure displayer.

--- src/DataRetriever.java
+++ src/DataRetriever.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -54,7 +55,8 @@ public class DataRetriever {
 			}
 		} catch (DataRetriever.UnauthorizedException e) {
 			e.printStackTrace();
-			;
+		} catch (NotFoundException e) {
+			e.printStackTrace();
 		}
 
 		// Typical result:
@@ -77,6 +79,8 @@ public class DataRetriever {
 		} catch (DataRetriever.UnauthorizedException e) {
 			e.printStackTrace();
 			;
+		} catch (NotFoundException e) {
+			e.printStackTrace();
 		}
 
 		// Typical result:
@@ -124,8 +128,9 @@ public class DataRetriever {
 	 * @param _stopID
 	 * @return
 	 * @throws UnauthorizedException
+	 * @throws NotFoundException 
 	 */
-	public static List<Departure> getDeparturesLineAtStop(String _lineId, String _stopID) throws UnauthorizedException {
+	public static List<Departure> getDeparturesLineAtStop(String _lineId, String _stopID) throws UnauthorizedException, NotFoundException {
 
 		if (m_apiKey == null) {
 			throw new UnauthorizedException("You must first set a valid API key using DataRetriever.setAPIKey");
@@ -167,7 +172,7 @@ public class DataRetriever {
 						arr.getJSONObject(i).getString("schedule")));
 			}
 		}
-
+		
 		return _departures;
 	}
 
@@ -188,7 +193,22 @@ public class DataRetriever {
 		}
 
 	}
+	
+	
+	public static class NotFoundException extends Exception {
 
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public NotFoundException(String _message) {
+			super(_message);
+		}
+
+	}
+
+	
 	/**
 	 * Execute a HTTP/1.1 GET method using the provided URL, with the current CSRF
 	 * token.
@@ -196,8 +216,9 @@ public class DataRetriever {
 	 * @param url
 	 * @return
 	 * @throws UnauthorizedException
+	 * @throws NotFoundException 
 	 */
-	private static String _executeGetRequest(URL url) throws UnauthorizedException {
+	private static String _executeGetRequest(URL url) throws UnauthorizedException, NotFoundException {
 		HttpsURLConnection connection = null;
 		StringBuffer response = new StringBuffer();
 
@@ -210,6 +231,10 @@ public class DataRetriever {
 
 			// Include referrer site
 			connection.setRequestProperty("Referer", m_csrfURL);
+			
+			// Accept gzip encoding
+			connection.setRequestProperty("Accept-encoding", "gzip, deflate");
+			connection.setRequestProperty("Accept-charset", "utf-8");
 
 			// Include CSRF Token
 			connection.setRequestProperty("X-CSRFToken", m_csrfToken);
@@ -223,10 +248,21 @@ public class DataRetriever {
 				is = connection.getInputStream();
 			} else if (HttpsURLConnection.HTTP_UNAUTHORIZED == connection.getResponseCode()) {
 				throw new UnauthorizedException("The server returned 401 Unauthorized. Maybe your API key is invalid?");
+			} else if (HttpsURLConnection.HTTP_NOT_FOUND == connection.getResponseCode()) {
+				throw new NotFoundException("The server returned 404 Not Found. The distant server might experience issues.");
 			} else {
 				is = connection.getErrorStream();
 			}
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			
+			// Handle gzip encoding
+			BufferedReader rd = null;
+			if (connection.getHeaderField("Content-Encoding")!=null && connection.getHeaderField("Content-Encoding").equals("gzip")) {
+				rd = new BufferedReader(new InputStreamReader(new GZIPInputStream(is)));
+			} else {
+				rd = new BufferedReader(new InputStreamReader(is));
+			}
+			
+			// Read answer line by line
 			String line;
 			while ((line = rd.readLine()) != null) {
 				response.append(line);
@@ -249,7 +285,6 @@ public class DataRetriever {
 	 */
 	private static void _refreshCSRFToken() {
 		HttpsURLConnection connection = null;
-		StringBuffer response = new StringBuffer();
 
 		try {
 			System.err.println("Fetching CSRF at: " + m_csrfURL);
@@ -260,6 +295,7 @@ public class DataRetriever {
 			// Include referrer site
 			connection.setRequestProperty("Referer", m_csrfURL);
 
+			// Accept gzip encoding
 			connection.setRequestProperty("Accept-encoding", "gzip, deflate");
 			connection.setRequestProperty("Accept-charset", "utf-8");
 
@@ -268,23 +304,10 @@ public class DataRetriever {
 
 			// Get cookies
 			m_cookies = connection.getHeaderFields().get("Set-Cookie");
+			
+			// Get CSRF token
 			m_csrfToken = ((m_cookies.get(0)).split(";")[0]).split("=", 2)[1];
 			System.err.println("Detected token: " + m_csrfToken);
-
-			// Read answer data
-			InputStream is = null;
-			if (HttpsURLConnection.HTTP_OK == connection.getResponseCode()) {
-				is = connection.getInputStream();
-			} else {
-				is = connection.getErrorStream();
-			}
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
